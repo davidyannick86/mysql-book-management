8260540dd8920935f6c8834cfa745b6c14b99f86
DavidYannick <57354908+davidyannick86@users.noreply.github.com>
2025-01-10 18:00:20 +0100

    Add GetBookById and CreateBook functions for book management

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -2,9 +2,13 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/davidyannick86/mysql-book-management/pkg/models"
+	"github.com/davidyannick86/mysql-book-management/pkg/utils"
+	"github.com/gorilla/mux"
 )
 
 var NewBook models.Book
@@ -14,5 +18,30 @@ func GetBooks(w http.ResponseWriter, r *http.Request) {
 	res, _ := json.Marshal(newBooks)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	Id, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println()
+		return
+	}
+	bookDetails, _ := models.GetBookById(Id)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	CreateBook := &models.Book{}
+	utils.ParseBody(r, CreateBook)
+	b := CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
 	w.Write(res)
 }
