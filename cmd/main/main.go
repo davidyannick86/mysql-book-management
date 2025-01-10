620b64ed5e8241f2361c0ec0edaec67210c98b8b
DavidYannick <57354908+davidyannick86@users.noreply.github.com>
2025-01-10 22:49:20 +0100

    Move database connection testing to utils package and update main function to call it

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,35 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/davidyannick86/mysql-book-management/pkg/routes"
 	"github.com/gorilla/mux"
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
-
-func testDBConnection() {
-	// * Define the connection string
-	dsn := "root:password@tcp(localhost:3306)/mysqldb?charset=utf8mb4&parseTime=True&loc=Local"
-
-	// * Open the connection
-	db, err := gorm.Open("mysql", dsn)
-	if err != nil {
-		log.Fatalf("Could not connect to the database: %v", err)
-	}
-	defer db.Close()
-
-	// * Test the connection
-	err = db.DB().Ping()
-	if err != nil {
-		log.Fatalf("Could not ping the database: %v", err)
-	}
-
-	fmt.Println("Successfully connected to the database!")
-}
 
 func main() {
 	// * Create a new router
@@ -43,5 +21,5 @@ func main() {
 
 	// * Listen and serve
 	log.Fatal(http.ListenAndServe("0.0.0.0:9010", nil))
-	//testDBConnection()
+	//utils.TestDBConnection()
 }
--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -2,8 +2,12 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
+	"log"
 	"net/http"
+
+	"github.com/jinzhu/gorm"
 )
 
 // * Parse the request body
@@ -19,4 +23,24 @@ func ParseBody(r *http.Request, model interface{}) error {
 		return err
 	}
 	return nil
+}
+
+func TestDBConnection() {
+	// * Define the connection string
+	dsn := "root:password@tcp(localhost:3306)/mysqldb?charset=utf8mb4&parseTime=True&loc=Local"
+
+	// * Open the connection
+	db, err := gorm.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("Could not connect to the database: %v", err)
+	}
+	defer db.Close()
+
+	// * Test the connection
+	err = db.DB().Ping()
+	if err != nil {
+		log.Fatalf("Could not ping the database: %v", err)
+	}
+
+	fmt.Println("Successfully connected to the database!")
 }
