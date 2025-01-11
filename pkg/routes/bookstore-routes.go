1c3d2cda2c6ba0bb7ecc3f6abc58ba14131ff878
DavidYannick <57354908+davidyannick86@users.noreply.github.com>
2025-01-11 08:27:42 +0100

    Remove commented-out hello route from bookstore routes

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -1,18 +1,15 @@
 package routes
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/davidyannick86/mysql-book-management/pkg/controllers"
 	"github.com/gorilla/mux"
 )
 
 // * Defining the routes for the bookstore
 var RegisterBookStoreRoutes = func(router *mux.Router) {
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello")
-	}).Methods("GET")
+	// router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprintf(w, "hello")
+	// }).Methods("GET")
 	router.HandleFunc("/book", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book", controllers.GetBook).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
